da7dd74d586a2ed4aeac48b6e6d87ee62af51d03
Sigurd Drivstuen <sigdriv06@gmail.com>
2024-04-23 11:15:26 +0200

    Add custom 404 page
    Add custom 404 – Page not found page

---
+++ app/not-found.tsx
@@ -0,0 +1,22 @@
+import Link from "next/link";
+
+export default function NotFound() {
+  return (
+    <main className=" pt-24 justify-center text-center">
+      <div>
+        <h1 className="text-3xl pb-20">404 – Page not found</h1>
+        <h2 className="text-3xl">
+          Det var et problem med å finne siden du leter etter
+        </h2>
+      </div>
+      <div>
+        <p className="text-xl pt-2">
+          Gå tilbake til{" "}
+          <Link href="/min-side" className="text-blue-600 hover:underline">
+            Min Side
+          </Link>
+        </p>
+      </div>
+    </main>
+  );
+}
