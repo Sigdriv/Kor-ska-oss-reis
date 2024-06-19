f306e7627c085835166a83fb8f8dee3f139829fa
Sigurd Drivstuen <sigdriv06@gmail.com>
2024-06-19 22:13:26 +0200

    Fix text center on admin dashboard

--- Frontend/app/(protected)/admin/page.tsx
+++ Frontend/app/(protected)/admin/page.tsx
@@ -6,7 +6,7 @@ export default function Admin() {
   return (
     <main className="">
       <div className=" pt-10 text-2xl">
-        <h1>Admin dashboard</h1>
+        <h1 className=" text-center">Admin dashboard</h1>
         <div className="flex flex-row gap-2 ml-24">
           <PaamelteCard />
           {/* <SendEmailCard /> */}
